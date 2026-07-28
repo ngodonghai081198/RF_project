bf229d1926bda4ba70dde9253afa364ae108c07c
ngodonghai081198 <haingodong1998@gmail.com>
2026-07-28 15:32:26 +0700

    first commit

---
+++ RF_project.srcs/sources_1/new/top_module.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/25/2026 08:50:52 PM
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+
+    );
+endmodule
